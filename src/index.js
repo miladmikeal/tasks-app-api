b1f7af54a849ad4149706985a90b75415bb8f0aa
Milad Mikeal <miladmikeal@gmail.com>
2019-06-01 22:17:07 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,83 @@
+const express = require('express');
+require('./db/mongoose');
+const User = require('./models/user');
+const Task = require('./models/task');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.post('/users', async (req, res) => {
+  const user = new User(req.body);
+
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (err) {
+    res.status(400).send();
+  }
+});
+
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(500).send();
+  }
+});
+
+app.get('/users/:id', async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.send(user);
+  } catch (err) {
+    res.status(500).send()
+  }
+});
+
+app.post('/tasks', async (req, res) => {
+  const task = new Task(req.body);
+
+  try {
+    await task.save();
+    res.send(task);
+  } catch (err) {
+    res.status(400).send()
+  }
+});
+
+app.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch (err) {
+    res.send(404).send()
+  }
+});
+
+app.get('/tasks/:id', async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const task = await Task.findById(_id);
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.send(task);
+  } catch (err) {
+    res.status(500).send();
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
