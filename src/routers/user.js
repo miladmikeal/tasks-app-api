eb5b5a244027f3abe542ef402685013f14468abd
Milad Mikeal <miladmikeal@gmail.com>
2019-06-02 19:17:04 -0700

    Add login route

--- src/routers/user.js
+++ src/routers/user.js
@@ -14,6 +14,15 @@ router.post('/users', async (req, res) => {
   }
 });
 
+router.post('/users/login', async (req, res) => {
+  try {
+    const user = await User.findByCredentials(req.body.email, req.body.password);
+    res.send(user);
+  } catch (err) {
+    res.status(400).send();
+  }
+});
+
 router.get('/users', async (req, res) => {
   try {
     const users = await User.find({});
@@ -41,7 +50,6 @@ router.get('/users/:id', async (req, res) => {
 router.patch('/users/:id', async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ['name', 'email', 'password', 'age'];
-  const _id = req.params.id;
 
   const isValidOperation = updates.every(update => allowedUpdates.includes(update));
 
@@ -50,10 +58,11 @@ router.patch('/users/:id', async (req, res) => {
   }
 
   try {
-    const user = await User.findByIdAndUpdate(_id, req.body, {
-      new: true,
-      runValidators: true
-    });
+    const user = await User.findById(req.params.id);
+
+    updates.forEach(update => user[update] = req.body[update]);
+
+    await user.save();
 
     if (!user) {
       return res.status(404).send();
